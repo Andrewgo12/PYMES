9922720b0ad7ce95e646b659d791522a40179cc9
Andres Gonzalez <147012730+Andrewgo12@users.noreply.github.com>
2025-11-27 11:49:59 -0500

    Fix: Remove scale transform causing React DOM errors

--- Pymes/src/components/layout/DashboardLayout.tsx
+++ Pymes/src/components/layout/DashboardLayout.tsx
@@ -7,9 +7,7 @@ export function DashboardLayout() {
       <Sidebar />
       <main className="flex-1 lg:ml-64">
         <div className="p-4 lg:p-6 max-w-full">
-          <div className="scale-[0.85] origin-top-left w-[117.65%]">
-            <Outlet />
-          </div>
+          <Outlet />
         </div>
       </main>
     </div>
